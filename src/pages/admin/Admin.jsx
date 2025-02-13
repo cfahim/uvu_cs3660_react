f913b3152a417f957d05033bff1c7b9ac7280a5c
Chris Fahim <6053415+cfahim@users.noreply.github.com>
2025-02-13 02:37:23 -0700

    added context to admin page

--- src/pages/admin/Admin.jsx
+++ src/pages/admin/Admin.jsx
@@ -1,10 +1,13 @@
-import { React } from "react";
+import { useContext, React } from "react";
 import MainLayout from "../layouts/MainLayout";
+import { AuthContext } from "../../context/AuthContext";
 
 const Admin = () => {
+    const { token } = useContext(AuthContext);
+
     return (
         <MainLayout title="Admin | MyPage">
-            <h2>Welcome Admin User</h2>
+            <h2>Welcome {token.username}</h2>
         </MainLayout>
     );
 };
